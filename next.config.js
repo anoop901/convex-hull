d4c61af254ffcb841cfb7c85481ef0f6f1b8ceb4
Anoop Naravaram <anoop901@gmail.com>
2023-07-19 01:34:11 -0700

    redirect root to basePath

--- next.config.js
+++ next.config.js
@@ -1,6 +1,16 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   basePath: "/convex-hull",
+  async redirects() {
+    return [
+      {
+        source: "/",
+        destination: "/convex-hull",
+        basePath: false,
+        permanent: true,
+      },
+    ];
+  },
 };
 
 module.exports = nextConfig;
